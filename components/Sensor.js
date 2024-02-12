96ea2258951217aba08df9a59c040de414ad4d9f
SujayAmberkar <sujayamberkar@gmail.com>
2024-02-13 00:19:23 +0530

    get gyroscopic data

--- components/Sensor.js
+++ components/Sensor.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native'
 import { Gyroscope } from 'expo-sensors';
 
 function Sensor() {
+  const [gyroscopeData, setGyroscopeData] = useState({ x: 0, y: 0, z: 0 });
+
+  useEffect(() => {
+    const subscription = Gyroscope.addListener(data => {
+      setGyroscopeData(data);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   return (
-    <Text>Sensor data</Text>
-  )
+    <>
+      <Text>Gyroscope Data:</Text>
+      <Text>X: {gyroscopeData.x}</Text>
+      <Text>Y: {gyroscopeData.y}</Text>
+      <Text>Z: {gyroscopeData.z}</Text>
+    </>
+  );
 }
 
 export default Sensor
