87e3e44a02bb563fb618ff9dfe41fe6ada505b4f
SujayAmberkar <sujayamberkar@gmail.com>
2024-02-12 23:43:30 +0530

    initial commit

---
+++ components/Sensor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { Gyroscope } from 'expo-sensors';
+
+function Sensor() {
+  return (
+    <Text>Sensor data</Text>
+  )
+}
+
+export default Sensor
