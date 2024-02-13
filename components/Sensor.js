24d631f4e85261f4b6a477bf93d1c95f8cbbe7b0
SujayAmberkar <sujayamberkar@gmail.com>
2024-02-13 18:05:19 +0530

    websocket implemented

--- components/Sensor.js
+++ components/Sensor.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native'
 import { Gyroscope } from 'expo-sensors';
+import WS from 'react-native-websocket'
+
 
 function Sensor() {
   const [gyroscopeData, setGyroscopeData] = useState({ x: 0, y: 0, z: 0 });
+  const [serverMsg, setServerMsg] = useState('not connected')
 
   useEffect(() => {
     const subscription = Gyroscope.addListener(data => {
       setGyroscopeData(data);
+      this.ws.send(JSON.stringify(data));
     });
 
     return () => {
@@ -17,6 +21,19 @@ function Sensor() {
 
   return (
     <>
+      <WS
+          ref={ref => {this.ws = ref}}
+          url="ws://192.168.0.104:8080/"
+          onOpen={() => {
+            console.log('Open!')
+            this.ws.send(JSON.stringify(gyroscopeData))
+            console.log(JSON.stringify(gyroscopeData));
+          }}
+          onMessage={console.log}
+          onError={console.log}
+          onClose={console.log}
+          reconnect // Will try to reconnect onClose
+        />
       <Text>Gyroscope Data:</Text>
       <Text>X: {gyroscopeData.x}</Text>
       <Text>Y: {gyroscopeData.y}</Text>
